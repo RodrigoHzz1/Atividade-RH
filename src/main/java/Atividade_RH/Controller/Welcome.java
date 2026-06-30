02d6209dc4662fc454d9a9a0659a4fb6ed6600d4
Rodrigo Costa <rodrigo.c.santos8@ba.estudante.senai.br>
2026-06-30 19:17:07 -0300

    Criação do EndPoint Welcome:Feat Juan França

---
+++ src/main/java/Atividade_RH/Controller/Welcome.java
@@ -0,0 +1,28 @@
+package Atividade_RH.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/")
+public class Welcome {
+    @GetMapping("/")
+    public Map<String, String> welcome() {
+        return Map.of(
+                "mensagem", "Bem-vindo à API da Atividade Pontuada!");
+
+
+    }
+
+
+    @GetMapping("/dev")
+    public Map<String, String> devInfo() {
+        return Map.of(
+                "desenvolvedores", "BackEnd:Rodrigo Costa// FrontEnd:Juan França"
+        );
+    }
+}
+
